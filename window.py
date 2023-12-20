640f9d8ef7d6b684b3e4a7ec7bac179c54648958
Callum Askew <154433436+Fizzchimp@users.noreply.github.com>
2023-12-20 17:51:51 +0000

    Create window.py

---
+++ window.py
@@ -0,0 +1,21 @@
+import turtle
+
+
+class Window:
+
+  def __init__(self, width, height):
+    self.screen = turtle.Screen()
+    self.screen.title("OOP Snake")
+    self.screen.bgcolor("teal")
+    self.screen.setup(width, height)
+    self.screen.tracer(0)
+
+    self.hud = turtle.Turtle()
+
+  def drawHud(self, score, highScore):
+    self.hud.clear()
+    self.hud.color("white")
+    self.hud.penup()
+    self.hud.hideturtle()
+    self.hud.goto(0, -390)
+    self.hud.write("Current Score: %d     High Score: %d" %(score, highScore), align = "center", font = ("Courier", 22, "normal"))
